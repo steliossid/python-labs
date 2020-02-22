0582e3e85a3f549368603b63028112f995312bce
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-02-22 18:42:05 +0100

    output_log implemented

--- Lab4/text_stats.py
+++ Lab4/text_stats.py
@@ -1,33 +1,21 @@
-# If second argument (a file name), output should be written to that file
-
 import sys
+import re
 import os.path
 from collections import Counter
-from string import ascii_lowercase, punctuation, digits
 
 
 def main():
     if len(sys.argv[1:]) >= 1:
         if os.path.isfile(sys.argv[1]):
-            print("**************************************************")
-            print("*** Welcome to a simple text extraction script ***")
-            print("**************************************************")
-            print("1) Frequency table for alphabetic letters")
-            print("2) Number of words")
-            print("3) Number of unique words")
-            print("4) Most common words")
-            choice = input("Enter the number you want (or the letter 'q' to quit):")
+            with open(sys.argv[1], "r", encoding="utf8") as file:
+                doc = data_processing(file)
+            letter_frequency(doc)
             print("\n")
-            while choice != "q":
-                with open(sys.argv[1], "r", encoding="utf8") as file:
-                    call_function(choice, file)
-                print("\n1) Frequency table for alphabetic letters")
-                print("2) Number of words")
-                print("3) Number of unique words")
-                print("4) Most common words")
-                choice = input("Enter the number you want (or the letter 'q' to quit):")
-                print("\n")
-            print("\nThank you for choosing the text extraction script.")
+            number_of_words(doc)
+            print("\n")
+            number_of_unique_words(doc)
+            print("\n")
+            common_words(doc)
         else:
             print("The file does not exist!")
             sys.exit(1)
@@ -36,85 +24,70 @@ def main():
         sys.exit(1)
 
 
-def call_function(choice, file):
-    if choice == "1":
-        frequency_alphabetic(file)
-    elif choice == "2":
-        number_of_words(file)
-    elif choice == "3":
-        number_of_unique_words(file)
-    elif choice == "4":
-        common_words(file)
-    else:
-        print("Non valid number! Choose another one.\n")
+def output_log(line):
+    print(line)
+    if len(sys.argv[1:]) == 2:
+        with open(sys.argv[2], "a", encoding="utf8") as output:
+            output.write(line + "\n")
 
 
-# print frequency table for alphabetic letters, ordered from the most common to the least
-def frequency_alphabetic(file):
-    c = Counter(letter for line in file for letter in line.lower() if letter in ascii_lowercase)
-    print("Frequency table for alphabetic letters:")
+def data_processing(file):
+    shakespeare = file.read()
+    doc = shakespeare.replace('\n', ' ')
+    doc = re.sub(r"\d", " ", doc)  # remove all digits
+    doc = re.sub(r"[^\w\s]", " ", doc)  # remove punctuations
+
+    doc = re.sub(r"[_]", " ", doc)
+    doc = re.sub(r"\sd\s", " ", doc)
+    doc = re.sub(r"\ss\s", " ", doc)
+    doc = re.sub(r"\so\s", " ", doc)
+
+    doc = re.sub(r"\s+", " ", doc)  # substitute all spaces with a single space
+    doc = doc.lower()
+    doc = doc.strip()
+    return doc
+
+
+def letter_frequency(doc):
+    doc2 = tuple(re.sub(r'\s+', "", doc))
+    c = Counter(doc2)
+    title = "Frequency table for alphabetic letters:"
+    output_log(title)
     for letter in c.most_common():
-        print(f"{letter[0]} ({letter[1]} occurrences)")
+        content = f"{letter[0]} ({letter[1]} occurrences)"
+        output_log(content)
 
 
-# print the number of words that the text contains, according to some definition of words
-# Should be 884.421 total words
-def number_of_words(file):
-    # c = 0
-    # with open("english-words.txt", "r") as word_file:
-    #     english_words = set(word.strip().lower() for word in word_file)
-    #     for line in file:
-    #         for word in line.lower().split():
-    #             if word in english_words:
-    #                 c += 1
-    digits_punct = digits + punctuation + "‘’“”—"
-    remove_digits_punct = str.maketrans('', '', digits_punct)
-    c = Counter(word.translate(remove_digits_punct) for line in file for word in line.lower().split()
-                if word.startswith("www") != True and word.startswith("http") != True)
-
-    for key, value in c.items():
-        print(f"{key} ({value} occurrences)")
-
-    print(f"The total number of words is {sum(c.values())}")
+def number_of_words(doc):
+    words = doc.split()
+    c = Counter(words)
+    content = f"The total number of words is {sum(c.values())}"
+    output_log(content)
 
 
-# print the number of unique words that the text contains, according to this definition
-# Should be 28.829 unique word forms
-def number_of_unique_words(file):
-    # with open("english-words.txt", "r") as word_file:
-    #     english_words = set(word.strip().lower() for word in word_file)
-    #     c = Counter(word for line in file for word in line.lower().split() if word in english_words)
-    #     print(f"The number of unique words is {len(c)}")
-    digits_punct = digits + punctuation + "’“”—"
-    remove_digits_punct = str.maketrans('', '', digits_punct)
-    c = Counter(word.translate(remove_digits_punct) for line in file for word in line.lower().split())
-
-    print(f"The number of unique words is {len(c)}")
+def number_of_unique_words(doc):
+    words = doc.split()
+    c = Counter(words)
+    content = f"The number of unique words is {len(c)}"
+    output_log(content)
 
 
-# print five most common words with their frequency + words that commonly follow them
-# (3 per word, ordered, number of occurrences)
-# Should be the following table:
-# the : 28.944
-# and : 27,317
-# i : 21,120
-# to : 20,136
-# of : 17,181
-def common_words(file):
-    # with open("english-words.txt", "r") as word_file:
-    #     english_words = set(word.strip().lower() for word in word_file)
-    #     c = Counter(word for line in file for word in line.lower().split() if word in english_words)
-    #     print("The five most common words are: ")
-    #     sorted_most_common = c.most_common()
-    #     for i in range(5):
-    #         print(f"{sorted_most_common[i][0]} ({sorted_most_common[i][1]} occurrences)")
-    digits_punct = digits + punctuation + "’“”—"
-    remove_digits_punct = str.maketrans('', '', digits_punct)
-    c = Counter(word.translate(remove_digits_punct) for line in file for word in line.lower().split())
-    print("The five most common words are: ")
-    sorted_most_common = c.most_common()
-    for i in range(5):
-        print(f"{sorted_most_common[i][0]} ({sorted_most_common[i][1]} occurrences)")
+def common_words(doc):
+    words = doc.split()
+    c = Counter(words)
+    five_most_common = dict(c.most_common(5))
+    title = "The five most common words are: "
+    output_log(title)
+    for key, value in five_most_common.items():
+        rx = r'%s (\w+ ){1}' % key
+        prog = re.compile(rx)
+        c2 = Counter(prog.findall(doc))
+        three_most_common = dict(c2.most_common(3))
+        content = f"{key} ({value} occurrences)"
+        output_log(content)
+        for key, value in three_most_common.items():
+            content2 = f"-- {key}, {value}"
+            output_log(content2)
 
 
 if __name__ == '__main__':
