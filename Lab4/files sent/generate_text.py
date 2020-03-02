50a2d9bfb5392eb4616942da717e4e0a9217dbaa
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-03-02 22:24:40 +0100

    lab4/file sent/ added

---
+++ Lab4/files sent/generate_text.py
@@ -0,0 +1,65 @@
+import numpy as np
+import time
+import sys
+import collections
+import text_stats
+
+
+def main():
+    with open(sys.argv[1], "r", encoding="utf8") as file:
+        doc = text_stats.data_processing(file)
+    sentence = generate_random_sentence(doc)
+    print(sentence)
+
+
+def give_next_word(possible_words):
+    """Chooses the next word with some probability from the frequency of each word
+
+    Arguments:
+    possible_words (dict): a counter/dictionary that contains the next possible words
+
+    Returns:
+    nxt_word (str): The chosen next word
+    """
+    possible_words = collections.OrderedDict(possible_words)  # keep the order of the dictionary
+    word_occ = np.fromiter(possible_words.values(), dtype=float)
+    prob_occ = word_occ / sum(word_occ)  # the probabilities of each word to be after
+    ref_dict = dict(zip(range(1, len(possible_words) + 1), possible_words.keys()))
+    indx = np.random.choice(a=np.array(range(1, len(possible_words) + 1)), p=prob_occ)  # get a weigted random next word
+    nxt_word = ref_dict[indx]
+    return nxt_word
+
+
+def generate_random_sentence(doc):
+    """
+    Generates a random sentence based on the successors of words
+
+    Arguments:
+    doc: The cleaned text in a string
+
+    Returns:
+    The random text sequence (random sentence)
+    """
+    ith_word = 1
+    max_wrds = int(sys.argv[3])
+    current_word = sys.argv[2]
+    gen_text = current_word
+    possible_words = [0, 0]
+    words = doc.split()
+
+    if current_word in words:
+        while len(possible_words) >= 1 and ith_word < max_wrds:
+            possible_words = text_stats.find_successors(doc, current_word)
+            nxt = give_next_word(possible_words)
+            gen_text = gen_text + " " + nxt[:-1]
+            current_word = nxt[:-1]
+            ith_word += 1
+
+    return gen_text
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    passed_time = round(time.time() - start_time)
+    print(f"{passed_time} seconds")
---
+++ Lab4/files sent/text_stats.py
@@ -0,0 +1,145 @@
+import sys
+import re
+import os.path
+from collections import Counter
+
+
+def main():
+    if len(sys.argv[1:]) >= 1:
+        if os.path.isfile(sys.argv[1]):
+            with open(sys.argv[1], "r", encoding="utf8") as file:
+                doc = data_processing(file)
+            letter_frequency(doc)
+            print("\n")
+            number_of_words(doc)
+            print("\n")
+            number_of_unique_words(doc)
+            print("\n")
+            common_words(doc)
+        else:
+            print("The file does not exist!")
+            sys.exit(1)
+    else:
+        print("No argument provided. Please provide at least the shakespeare.txt.")
+        sys.exit(1)
+
+
+def output_log(line):
+    """
+    Print a line and if output file argument is given, write that line to the file
+
+    Arguments:
+    line: A type string variable
+    """
+    print(line)
+    if len(sys.argv[1:]) == 2:
+        with open(sys.argv[2], "a", encoding="utf8") as output:
+            output.write(line + "\n")
+
+
+def data_processing(file):
+    """Clean a txt and keep only words seperated by commas
+
+    Arguments:
+    file : the file that will clean
+
+    Returns:
+    doc (str): The cleaned text in a string
+    """
+    doc = file.read()
+    doc = doc.replace('\n', ' ')
+    doc = re.sub(r"\d", " ", doc)  # remove all digits
+    doc = re.sub(r"[^\w\s]", " ", doc)  # remove punctuations
+
+    doc = re.sub(r"[_]", " ", doc)
+    doc = re.sub(r"\sd\s", " ", doc)
+    doc = re.sub(r"\ss\s", " ", doc)
+    doc = re.sub(r"\so\s", " ", doc)
+
+    doc = re.sub(r"\s+", " ", doc)  # substitute all spaces with a single space
+    doc = doc.lower()
+    doc = doc.strip()
+    return doc
+
+
+def letter_frequency(doc):
+    """
+    Calculates how many times each letter appears in the text
+
+    Arguments:
+    doc (str): The cleaned text in a string
+    """
+    doc2 = tuple(re.sub(r'\s+', "", doc))
+    c = Counter(doc2)
+    title = "Frequency table for alphabetic letters:"
+    output_log(title)
+    for letter in c.most_common():
+        content = f"{letter[0]} ({letter[1]} occurrences)"
+        output_log(content)
+
+
+def number_of_words(doc):
+    """
+    Calculates how many words there are in the text in total
+
+    Arguments:
+    doc (str): The cleaned text in a string
+    """
+    words = doc.split()
+    c = Counter(words)
+    content = f"The total number of words is {sum(c.values())}"
+    output_log(content)
+
+
+def number_of_unique_words(doc):
+    """
+    Calculates how many unique words there are in the text
+
+    Arguments:
+    doc (str): The cleaned text in a string
+    """
+    words = doc.split()
+    c = Counter(words)
+    content = f"The number of unique words is {len(c)}"
+    output_log(content)
+
+
+def find_successors(text, word):
+    """Find the successors
+
+    Arguments:
+    text (str): the text that will search in
+    word (str): a word of the text to find the next possible words
+
+    Returns:
+    possible_words (dict): The next possible words with their frequency in a dictionary
+    """
+    rx = r'%s (\w+ ){1}' % word
+    prog = re.compile(rx)
+    possible_words = Counter(prog.findall(text))
+    return possible_words
+
+def common_words(doc):
+    """
+    Finds the five most common words and the three most common successors of these five words.
+
+    Arguments:
+    doc (str): The cleaned text in a string
+    """
+    words = doc.split()
+    c = Counter(words)
+    five_most_common = dict(c.most_common(5))
+    title = "The five most common words are: "
+    output_log(title)
+    for key, value in five_most_common.items():
+        c2 = find_successors(doc, key)
+        three_most_common = dict(c2.most_common(3))
+        content = f"{key} ({value} occurrences)"
+        output_log(content)
+        for key, value in three_most_common.items():
+            content2 = f"-- {key}, {value}"
+            output_log(content2)
+
+
+if __name__ == '__main__':
+    main()
